7fb8f92e754ceeb3f0122bbb0886a015593b970e
B Kuz <borka.11121@gmail.com>
2021-10-07 08:25:11 -0700

    Shifting more functions in to bookutils.py common lib

--- common/bookutils.py
+++ common/bookutils.py
@@ -1,6 +1,41 @@
 import os
+import sys
+from bs4 import BeautifulSoup
+import distutils.dir_util
+import re
 
 ENC = 'utf-8'
+
+'''
+Copies a folder and all contents
+into another folder, overwriting existing
+
+Example:
+if you have folder /a/b/c/
+and you want to copy 'c' and contents to
+path /a/d/e/f/,
+  copy_folder_recursively('a/b/c/', 'a/d/e/f/')
+results in:
+    /a/d/e/f/c/
+'''
+def copy_folder_recursively(src, dest):
+    src_dir = src
+    print("src dir: " + src_dir)
+    # need to get top level folder name to construct dest path
+    # (i.e. if 'a/b/c' want 'c')
+    # os.path.basename will return empty string if path ends in '/' char
+    # so strip it off if its there
+    if src_dir.endswith("/"):
+        src_dir = ASSETS_DIR[:-1] # strips off last char of string
+    print("src dir: " + src_dir)
+    src_foldername = os.path.basename(src_dir)
+    dest_dir = os.path.abspath(os.path.join(dest, src_foldername))
+    print("dest dir: " + dest_dir)
+    print(("Copy {} to {}").format(src_dir, dest_dir))
+    # shutil will fail if the dest dir exists
+    #shutil.copytree(src_dir, dest_dir)
+    # https://stackoverflow.com/questions/10047521/how-to-copy-directory-with-all-file-from-c-xxx-yyy-to-c-zzz-in-python/17358075
+    distutils.dir_util.copy_tree(src_dir, dest_dir)
 
 '''
 Takes an abs path to a data file for a book,
@@ -74,4 +109,58 @@ i.e. :myChap:/filepath/file.txt)''').format(i, bookinput_filepath))
 
     return title, chapter_data
 
+'''
+Get contents of a text file as a string
+and return the string
+'''
+def get_file_as_str(filepath):
+    print(("\t\tRead file: {}").format(filepath))
+    html_file = open(filepath, "r", encoding=ENC)
+    html_as_str = html_file.read()
+    html_file.close()
+    return html_as_str
 
+'''
+return a BeautifulSoup object
+given a strong of html data
+'''
+def make_soup(html_str):
+    soup = BeautifulSoup(html_str.encode(ENC), 'html.parser')
+    return soup
+
+'''
+take a BeautifulSoup object an an abs path
+and writes to an html file
+at the specified path
+fails if not an abs path.
+'''
+def write_soup_to_file(soup, output_filename, force):
+    # write to html
+    soup = soup.prettify(formatter='html')
+
+    if not os.path.isabs(output_filename):
+        sys.exit("ERROR: 'write_soup_to_file' - output_filename is not absolute!")
+
+    if os.path.exists(output_filename) and not force:
+        sys.exit(('''\n
+ERROR! An output file, {}, already exists!
+Solutions:\n
+\t1. Specify different output dir using --output\n
+\t2. Give --timestamp flag\n
+\t3. Give --FORCE option (will overwrite existing files, be careful''').format(output_filename))
+
+    # create dirs in path if they don't exist
+    basedir = os.path.dirname(output_filename)
+    if not os.path.exists(basedir):
+        print(("\t\tbookutils: Make dirs in path : {}").format(basedir))
+        os.makedirs(basedir)
+
+    if force:
+        print(("\tbookutils: Write file {} ('force' given; overwrite existing)...").format(output_filename))
+        wr = open(output_filename, "w", encoding=ENC)
+    else:
+        print(("\tbookutil: Write file {} ...").format(output_filename))
+        wr = open(output_filename, "x", encoding=ENC)
+    wr.write(str(soup))
+    wr.close()
+    return output_filename
