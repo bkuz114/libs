991c8e9a83952355bda924e0c5a847fc3a13dae6
BIK <bikz1199@gmail.com>
2022-10-29 11:47:19 -0700

    logging: introduce logging module to do color logging
    Issue: https://github.com/bkuz114/book-builder/issues/12
    Project: https://github.com/users/bkuz114/projects/3
    
    context:
    --------
    
    This module was created to be used by the book_builder.py
    tool and associated modules.
    
    overview:
    ----------
    
    	Basic usage:
    	------------
    
    	import libs.colorlogger as colorlogger
    
    	colorlogger.setup()
    
    	How it works:
    	------------
    
    	This module comes with a function called 'setup()'
    	When you call the 'setup()' function,
    	it uses python's built in logging module to configure
    	a root logger, which can print to both console and optional log files.
    	It prints to the console in color - different colors for different log levels.
    	setup() has various options, such as what log level to use, filepath for
    	a colored log and a non colored log, etc.
    
    	How to use this in a project with multiple modules
    	---------------------
    
    	Suppose you have a project which has a main entry point, but also calls
    	lots of other modules.
    
    	You would only need to call this .setup() function once for the life of your run -
    	ideally, in your main entrypoint at the start of your project run.
    
    	Then, the remaining modules would just need to import logging and
    	and get a logger.
    
    	Example:
    
    	** in your main.py **:
    
    	import logging # in EVERY module
    	import libs.colorlogger as colorlogger # ONLY once.
    
    	# only call setup ONCE for entire project.
    	# set up to have your desired log level, logfiles, etc.
    	colorlogger.setup(loglevel_console=logging.INFO, loglevel_logfile=logging.DEBUG, logfile_color="myfile.txt")
    	# now root logger is set up for entire app run...
    
    	# boilerplate code that will go in every module of your project:
    	logger = logging.getLogger(__name__)
    	print = logger.info # now you don't need to rename existing print statement - they will all go to logger.info
    
    	print("hello!")
    	logging.debug("a debug line")
    	logging.info("an info line - this is identical to what print calls now")
    
    	** in your remaining modules **:
    
    	import logging
    	logger = logging.getLogger(__name__)
    	print = logger.info
    
    	print("when I print now, I am getting the configuration that main.py set up for me.")
    	print("Note- If I'm running independent of main.py, I won't get any of that configuration; I'll just get logging lib's default logging (printing to console)")
    
    Why this is being
    put in the 'libs' repo:
    ------------------------
    
    	I'm not sure where this should end up.
    	I ended up putting it in libs/ because I want to be able to use logging
    	in beautiful_soup_utils.py and io_utils.py which make some important
    	print statements in the run of book_builder.py.
    	So for now, seemed easiest to put it here. This is subject to change.

---
+++ colorlogger.py
@@ -0,0 +1,105 @@
+'''
+Sets up root logger of python's logging module
+so that it logs to console and a logfile,
+and colors console output based on the log level.
+
+usage:
+
+    import utils.colorlogger as colorLogger
+
+    colorLogger.setup()
+    colorLogger.test()
+
+'''
+
+import sys
+import logging
+import io_utils
+
+# some formats to use
+FORMAT_LOGFILE = "%(asctime)s:: %(levelname)s: %(message)s (%(filename)s:%(lineno)d)"
+FORMAT_CONSOLE_BASIC = "%(message)s"
+FORMAT_CONSOLE_HIGH = "%(levelname)s: %(message)s (%(filename)s:%(lineno)d)"
+
+'''
+custom logging Formatter to display
+log levels in different colors and formatts
+https://stackoverflow.com/questions/384076/how-can-i-color-python-logging-output
+'''
+class CustomFormatter(logging.Formatter):
+
+    green ="\x1b[32;20m"
+    blue = "\x1b[34;20m"
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+
+    FORMATS = {
+        logging.DEBUG: green + FORMAT_CONSOLE_BASIC + reset,
+        logging.INFO: blue + FORMAT_CONSOLE_BASIC + reset,
+        logging.WARNING: yellow + FORMAT_CONSOLE_HIGH + reset,
+        logging.ERROR: red + FORMAT_CONSOLE_HIGH + reset,
+        logging.CRITICAL: bold_red + FORMAT_CONSOLE_HIGH + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+'''
+set up root logger
+
+Arguments:
+    @loglevel: what level to log from
+    @logfile_nocolor: path of colorless logfile
+        (rel script importing this; if none, don't make one.)
+    @logfile_color: path of colorful logfile
+        ("" "")
+'''
+def setup(loglevel_console=logging.DEBUG, loglevel_logfile=logging.DEBUG, logfile_nocolor=None, logfile_color=None):
+    '''
+    create handlers (console and logfile)
+    '''
+    handlers = []
+
+    # console handler
+    ch = logging.StreamHandler(sys.stdout) # if you don't do this, will print to stderr
+    ch.setFormatter(CustomFormatter())
+    ch.setLevel(loglevel_console)
+    handlers.append(ch)
+
+    # logfile handlers
+    # first logfile (prints full date, suppresses color)
+    if logfile_nocolor:
+        io_utils.createPath(logfile_nocolor) # parent dir must exist of logging will fail
+        fh = logging.FileHandler(logfile_nocolor, "w")
+        fh.setFormatter(logging.Formatter(FORMAT_LOGFILE))
+        fh.setLevel(loglevel_logfile)
+        handlers.append(fh)
+    # second logfile (same format as console handler; cat to see the color)
+    if logfile_color:
+        io_utils.createPath(logfile_color)
+        fh2 = logging.FileHandler(logfile_color, "w")
+        fh2.setFormatter(CustomFormatter())
+        fh2.setLevel(loglevel_logfile)
+        handlers.append(fh2)
+
+    # set basic configuration for root logger
+    logging.basicConfig(
+            #level=logging.DEBUG,
+            #format='%(asctime)s,%(msecs)d %(levelname)s %(message)s (from file: %(name)s)',
+            datefmt='%H:%M:%S',
+            handlers=handlers
+        )
+
+def test():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    logger.debug("debug message")
+    logger.info("info message")
+    logger.warning("warning message")
+    logger.error("error message")
+    logger.critical("critical message")
