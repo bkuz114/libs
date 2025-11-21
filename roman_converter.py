9d901f87b5fb23cf549adfdbc8781a5a4152e150
BIK <bikz1199@gmail.com>
2025-11-20 20:21:56 -0800

    Cleanup roman_converter.py
    - flake8 for PEP8 style guidelines
    - If a number doesn't have a roman equivalent (<=0), return None
      rather than throwing a ValueError

--- roman_converter.py
+++ roman_converter.py
@@ -1,51 +1,47 @@
-import sys
-import re
+"""
+Module to convert from int to roman numerals,
+or roman numerals to int.
 
-'''
-Basic roman numeral <--> integer converter.
-Needed because roman py package fails on Windows.
+Usage:
+    python roman_convert.py ROMAN_OR_INT
 
-usage:
-    1. on cmd:
-        python roman_converter.py 5 --> prints V
-        python roman_converter.py X --> prints 10
-    2. in .py file
-        import roman_converter
-        roman_converter.int_to_roman(5) --> returns "V"
-        roman_converter.roman_to_int("X") --> returns 10
-
-----------------------
 int to roman:
     https://stackoverflow.com/questions/28777219/basic-program-to-convert-integer-to-roman-numerals
+
 roman to int:
     https://stackoverflow.com/questions/19308177/converting-roman-numerals-to-integers-in-python
+
 is_roman:
     https://dev.to/alexdjulin/a-python-regex-to-validate-roman-numerals-2g99
-is_digit:
-    https://stackoverflow.com/questions/28279732/how-to-type-negative-number-with-isdigit
+
 if all else fails:
     pip install roman
     https://pypi.org/project/roman/
-    (but it fails on Windows..., hence why I've made this)
-'''
+    (but it has an issue on Windows...)
+"""
+
+import sys
+import re
 
 ROMAN = [
     (1000, "M"),
-    ( 900, "CM"),
-    ( 500, "D"),
-    ( 400, "CD"),
-    ( 100, "C"),
-    (  90, "XC"),
-    (  50, "L"),
-    (  40, "XL"),
-    (  10, "X"),
-    (   9, "IX"),
-    (   5, "V"),
-    (   4, "IV"),
-    (   1, "I"),
+    (900, "CM"),
+    (500, "D"),
+    (400, "CD"),
+    (100, "C"),
+    (90, "XC"),
+    (50, "L"),
+    (40, "XL"),
+    (10, "X"),
+    (9, "IX"),
+    (5, "V"),
+    (4, "IV"),
+    (1, "I"),
 ]
 
+
 def is_roman(num):
+    """return True False if a value is a roman numeral"""
 
     pattern = re.compile(r"""
                                 ^M{0,3}
@@ -59,22 +55,11 @@ def is_roman(num):
 
     return False
 
-'''
-return True if n is an integer;
-False otherwise. (needed because
-python's isdigit() doesn't work
-for negative numbers)
-'''
-def is_digit(n):
-    try:
-        int(n) # throws err if number isn't digit
-        return True
-    except ValueError:
-        return  False
 
 def int_to_roman(number):
+    """Return roman numeral version of an int, or None if int <= 0"""
+
     if int(number) <= 0:
-        raise ValueError("Can't convert {} to roman numeral; there is no roman numeral for it".format(number))
         return None
 
     result = ""
@@ -83,22 +68,29 @@ def int_to_roman(number):
         result += roman * factor
     return result
 
+
 def roman_to_int(s):
+    """Return int version of a Roman numeral"""
+
     if not is_roman(s):
-        raise ValueError("{} is not a valid roman numeral".format(s))
+        return None
 
     d = {'m': 1000, 'd': 500, 'c': 100, 'l': 50, 'x': 10, 'v': 5, 'i': 1}
     n = [d[i] for i in s.lower() if i in d]
-    return sum([i if i>=n[min(j+1, len(n)-1)] else -i for j,i in enumerate(n)])
+    return sum([i if i >= n[min(j+1, len(n)-1)]
+                else -i for j, i in enumerate(n)])
+
 
 def main(args):
+    """Converts 1st cmd arg to py script (int -> roman or roman -> int)"""
     num = args[0]
     res = None
-    if is_digit(num):
+    if num.isdigit():
         res = int_to_roman(int(num))
     else:
         res = roman_to_int(num)
     print(res)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
