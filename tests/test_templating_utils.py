b9ffb6caf4edf57b8e2f307ff6908db26e8edba9
BIK <bikz1199@gmail.com>
2025-11-23 23:04:48 -0800

    Convert test_templating_utils.py to be run with pytest
    usage:
        virtualenv venv && source ./venv/Scripts/activate && pip install -r requirements.txt
        pytest test_templating_utils.py [-k 'TEST_A or TEST_B or ... TEST_N']
    
    Examples:
    
        1. Run all tests
            pytest test_templating_utils.py
    
        2. Run test_1 only
            pytest test_templating_utils.py -k 'test_1'
    
        3. Run test_1 and test_2 only
            pytest test_templating_utils.py -k 'test_1 or test_2'

--- tests/test_templating_utils.py
+++ tests/test_templating_utils.py
@@ -1,43 +1,128 @@
+"""
+Unit tests for templating_utils.py
+
+usage:
+    virtualenv venv && source ./venv/Scripts/activate && pip install -r requirements.txt
+    pytest test_templating_utils.py [-k 'TEST_A or TEST_B or ... TEST_N']
+
+Examples:
+
+    1. Run all tests
+        pytest test_templating_utils.py
+
+    2. Run test_1 only
+        pytest test_templating_utils.py -k 'test_1'
+
+    3. Run test_1 and test_2 only
+        pytest test_templating_utils.py -k 'test_1 or test_2'
+"""
+
 import sys
-import os
 sys.path.append("..")
-import templating_utils
+import templating_utils  # noqa: E402
 
-SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+inner_text1 = "these strings"
+inner_text2 = "are in"
+inner_text3 = "these"
+inner_text4 = "these that are"
+LD1 = "{"
+RD1 = "}"
+LD2 = "["
+RD2 = "]"
+encapsulated1 = LD1 + inner_text1 + RD1
+encapsulated2 = LD1 + inner_text2 + RD1
+encapsulated3 = LD2 + inner_text3 + RD2
+encapsulated4 = LD2 + inner_text4 + RD2
 
-to_template = '''
-First, I want to template {these strings} that {are in} curly brackets.
-Second, I want to template [these] and [these that are] in hard brackets.
-'''
-print("ORIGINAL TEXT:")
-print(to_template)
-
-print("\n ==== Test #1: Template {, } delims (no &nbsp; padding) ==== ")
-templated = templating_utils.template_string(
-        to_template, "{", "}", "span",
-        {"class": "a b c"}, False)
-print(templated)
-
-print("\n ==== Test #2: Template [, ] delims (no &nbsp; padding) ==== ")
-templated = templating_utils.template_string(
-        to_template, "[", "]", "span",
-        {"class": "a b c"}, False)
-print(templated)
-
-print("\n ==== Test #3: Template {, } delims (&nbsp; padding) ==== ")
-templated = templating_utils.template_string(
-        to_template, "{", "}", "span",
-        {"class": "a b c"}, True)
-print(templated)
-
-print("\n ==== Test #4: Template {, } and [, ] delims (&nbsp; padding) ==== ")
-templated = templating_utils.template_string(
-        to_template, "{", "}", "span",
-        {"class": "a b c"}, True)
-templated = templating_utils.template_string(
-        templated, "[", "]", "span",
-        {"class": "a b c"}, True)
-print(templated)
+ORIGINAL = '''
+First, I want to template {} that {} curly brackets.
+Second, I want to template {} and {} in hard brackets.
+'''.format(encapsulated1, encapsulated2, encapsulated3, encapsulated4)
 
 
+def test_1():
+    """test replacing {, } delims with no &nbsp; padding"""
+    print("\nTest #1: Template {}, {} delims (no &nbsp; padding)".format(
+        LD1, RD1))
+    print("Original:")
+    print(ORIGINAL)
+    classes_to_add = "a b c"
+    templated = templating_utils.template_string(
+        ORIGINAL, LD1, RD1, "span",
+        {"class": classes_to_add}, False)
+    print("Templated:")
+    print(templated)
+    expected_tag1 = '<span class="{}">{}</span>'.format(
+            classes_to_add, inner_text1)
+    expected = ORIGINAL.replace(encapsulated1, expected_tag1)
+    expected_tag2 = '<span class="{}">{}</span>'.format(
+            classes_to_add, inner_text2)
+    expected = expected.replace(encapsulated2, expected_tag2)
+    assert templated == expected
 
+
+def test_2():
+    """test replacing [, ] delims with no &nbsp; padding"""
+    print("\nTest #2: Template {}, {} delims (no &nbsp; padding)".format(
+        LD2, RD2))
+    print("Original:")
+    print(ORIGINAL)
+    classes_to_add = "a b c"
+    templated = templating_utils.template_string(
+        ORIGINAL, LD2, RD2, "span",
+        {"class": classes_to_add}, False)
+    print("Templated:")
+    print(templated)
+    expected_tag1 = '<span class="{}">{}</span>'.format(
+            classes_to_add, inner_text3)
+    expected = ORIGINAL.replace(encapsulated3, expected_tag1)
+    expected_tag2 = '<span class="{}">{}</span>'.format(
+            classes_to_add, inner_text4)
+    expected = expected.replace(encapsulated4, expected_tag2)
+    assert templated == expected
+
+
+def test_3():
+    """test replacing {, } delims with &nbsp; padding"""
+    print("\nTest #1: Template {}, {} delims (&nbsp; padding)".format(
+        LD1, RD1))
+    print("Original:")
+    print(ORIGINAL)
+    classes_to_add = "a b c"
+    templated = templating_utils.template_string(
+        ORIGINAL, LD1, RD1, "span",
+        {"class": classes_to_add}, True)
+    print("Templated:")
+    print(templated)
+    expected_tag1 = '<span class="{}">{}</span>'.format(
+            classes_to_add, inner_text1)
+    expected = ORIGINAL.replace(encapsulated1, expected_tag1)
+    expected_tag2 = '<span class="{}">{}</span>'.format(
+            classes_to_add, inner_text2)
+    expected = expected.replace(encapsulated2, expected_tag2)
+    expected = expected.replace(" <span", "&nbsp;<span")
+    expected = expected.replace("</span> ", "</span>&nbsp;")
+    assert templated == expected
+
+
+def test_4():
+    """test replacing [, ] delims with &nbsp; padding"""
+    print("\nTest #4: Template {}, {} delims (&nbsp; padding)".format(
+        LD2, RD2))
+    print("Original:")
+    print(ORIGINAL)
+    classes_to_add = "a b c"
+    templated = templating_utils.template_string(
+        ORIGINAL, LD2, RD2, "span",
+        {"class": classes_to_add}, True)
+    print("Templated:")
+    print(templated)
+    expected_tag1 = '<span class="{}">{}</span>'.format(
+            classes_to_add, inner_text3)
+    expected = ORIGINAL.replace(encapsulated3, expected_tag1)
+    expected_tag2 = '<span class="{}">{}</span>'.format(
+            classes_to_add, inner_text4)
+    expected = expected.replace(encapsulated4, expected_tag2)
+    expected = expected.replace(" <span", "&nbsp;<span")
+    expected = expected.replace("</span> ", "</span>&nbsp;")
+    assert templated == expected
