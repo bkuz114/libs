83c03543cd4faaf3e30d32b17c5869bd25da2226
BIK <bikz1199@gmail.com>
2025-11-24 20:05:44 -0800

    Convert test_declension_utils.py to run with pytest
    usage:
        virtualenv venv && source ./venv/Scripts/activate && pip install -r requirements.txt
        pytest test_declension_utils.py [-k 'TEST_A or TEST_B or ... TEST_N']
    
    Examples:
    
        1. Run all tests
            pytest test_declension_utils.py
    
        2. Run test_mark_string only
            pytest test_declension_utils.py -k 'test_mark_string'
    
        3. Run test_mark_string and test_mark_sentence_1
            pytest test_declension_utils.py -k 'test_mark_string or test_mark_sentence_1'
    
        4. Run all test_mark_sentence tests
        	pytest test_declension_utils.py -k 'test_mark_sentence'

---
+++ tests/pytest/test_declension_utils.py
@@ -0,0 +1,145 @@
+"""
+Test for test_declension_utils.py
+
+usage:
+    virtualenv venv && source ./venv/Scripts/activate && pip install -r requirements.txt
+    pytest test_declension_utils.py [-k 'TEST_A or TEST_B or ... TEST_N']
+
+Examples:
+
+    1. Run all tests
+        pytest test_declension_utils.py
+
+    2. Run test_mark_string only
+        pytest test_declension_utils.py -k 'test_mark_string'
+
+    3. Run test_mark_string and test_mark_sentence_1
+        pytest test_declension_utils.py -k 'test_mark_string or test_mark_sentence_1'
+
+    4. Run all the test_mark_sentence_* tests
+        pytest test_declension_utils.py -k 'test_mark_sentence'
+"""
+
+import sys
+sys.path.append("../..")
+import declension_utils as marker  # noqa: E402
+
+
+def test_mark_string():
+    """ test mark_string function """
+
+    print("\nTest 'mark_string' function")
+
+    sentence1 = "{[Иван*а] сын.}"
+    sentence2 = "{папа [Бор*и].}"
+    html_str = '''
+<html>
+    <head></head>
+    <body>
+        {}
+        <br>
+        {}
+    </body>
+</html>
+'''.format(sentence1, sentence2)
+    print("Original string:")
+    print(html_str)
+
+    print("Mark-up string in <span> tags")
+    marked = marker.mark_string(
+            html_str, "{", "}", "[", "]", "[[", "]]", "*")
+
+    expected = html_str.replace(
+            sentence1, "<span>Иван</span><span>а</span> сын.")
+    expected = expected.replace(
+            sentence2, "папа <span>Бор</span><span>и</span>.")
+
+    print("Marked up string:")
+    print(marked)
+    assert marked == expected
+
+
+def test_mark_sentence_1():
+    """ test mark_sentence, NO padding <span> with &nbsp; """
+
+    print("\nTest 'mark_sentence' function")
+
+    # sentence will be "Ивана сын."
+
+    # delimeters to use
+    noun_delim_l = "["
+    noun_delim_r = "]"
+    decline_delim = "*"
+    # noun broken into base + declension
+    noun_start = "Иван"
+    noun_declension = "а"
+    # rest of sentence
+    rest_of_sentence = " сын."
+    # the sentence to markup
+    sentence = "{}{}{}{}{}{}".format(
+            noun_delim_l, noun_start,
+            decline_delim, noun_declension,
+            noun_delim_r, rest_of_sentence)
+    print("Original sentence:")
+    print(sentence)
+
+    # expected markup of sentence
+    expected = "<span>{}</span><span>{}</span>{}".format(
+            noun_start, noun_declension, rest_of_sentence)
+
+    print("Mark up the sentence by calling mark_sentence")
+    # mark up the sentence
+    marked = marker.mark_sentence(
+            sentence,
+            noun_delim_l, noun_delim_r,
+            "[[", "]]",
+            decline_delim,
+            False, False)
+
+    print("Marked up sentence:")
+    print(marked)
+    assert marked == expected
+
+
+def test_mark_sentence_2():
+    """ test mark_sentence, padding <span> with &nbsp; """
+
+    print("\nTest 'mark_sentence' function, padding <span> with &nbsp;")
+
+    # sentence will be "Ивана сын."
+
+    # delimeters to use
+    noun_delim_l = "["
+    noun_delim_r = "]"
+    decline_delim = "*"
+    # noun broken into base + declension
+    noun_start = "Иван"
+    noun_declension = "а"
+    # rest of sentence
+    rest_of_sentence = " сын."
+    # the sentence to markup
+    sentence = "{}{}{}{}{}{}".format(
+            noun_delim_l, noun_start,
+            decline_delim, noun_declension,
+            noun_delim_r, rest_of_sentence)
+    print("Original sentence:")
+    print(sentence)
+
+    # expected markup of sentence
+    expected = "<span>{}</span><span>{}</span>{}".format(
+            noun_start, noun_declension, rest_of_sentence)
+    expected = expected.replace(" <span>", "&nbsp;<span>")
+    expected = expected.replace("</span> ", "</span>&nbsp;")
+
+    print("Mark up the sentence by calling mark_sentence")
+    # mark up the sentence
+    marked = marker.mark_sentence(
+            sentence,
+            noun_delim_l, noun_delim_r,
+            "[[", "]]",
+            decline_delim,
+            False, True)
+
+    print("Marked up sentence:")
+    print(marked)
+    assert marked == expected
